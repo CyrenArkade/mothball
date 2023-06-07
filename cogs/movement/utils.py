0644ceb0ea2a6f67e2240fd14b3f998e9e0410c4
Cyren <cyrenarkade@gmail.com>
2023-06-07 10:23:16 -0700

    minor fastmath fix

--- cogs/movement/utils.py
+++ cogs/movement/utils.py
@@ -21,3 +21,5 @@ pi = 3.14159265358979323846
 fastmath_sin_table = [0] * 4096
 for i in range(4096):
     fastmath_sin_table[i] = fl(sin((fl(i) + fl(0.5)) / fl(4096) * (fl(pi) * fl(2))))
+for i in range(4):
+    fastmath_sin_table[int(fl(i*90) * fl(11.377778)) & 4095] = fl(sin(fl(i*90) * fl(0.017453292)))
